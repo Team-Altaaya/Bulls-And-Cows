f5171494542d519882c0e35ed360eaaedeb5df66
Andrey Bozhilov <andrei.bozhilov@gmail.com>
2014-12-28 16:40:50 +0200

    add UnitTest project

---
+++ Bulls-and-Cows-Refactured/Interfaces/IRandomGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace BullsAndCowsGame.Interfaces
+{
+    public interface IRandomGenerator
+    {
+        string Generate(int numberLenght);
+
+        string GenerateNumberByDigits(int numberLenght, int digitMin, int digitMax);
+    }
+}
---
+++ BullsAndCowsTests/FakeClasses/FakeNumberGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BullsAndCowsGame.Interfaces;
+
+namespace BullsAndCowsTests.FakeClasses
+{
+    public class FakeNumberGenerator : IRandomGenerator
+    {
+        public FakeNumberGenerator(string generateNumber, string helpPattern)
+        {
+            this.GenerateNumber = generateNumber;
+            this.HelpPattern = helpPattern;
+        }
+
+        public string GenerateNumber { get; set; }
+
+        public string HelpPattern { get; set; }
+
+        public string Generate(int numberLenght)
+        {
+            return this.GenerateNumber;
+        }
+
+        public string GenerateNumberByDigits(int numberLenght, int digitMin, int digitMax)
+        {
+            return this.HelpPattern;
+        }
+    }
+}
