d218963812f9c9e2e9f6f55d87cd0e9c42e89ef8
luboslav1995 <milkovlubo@gmail.com>
2015-01-06 22:21:53 +0200

    Some bugs fixed and added PlayerEnteredIncorrectCommandTestMethod

--- BullsAndCowsTests/GameEngineTests.cs
+++ BullsAndCowsTests/GameEngineTests.cs
@@ -20,12 +20,6 @@ namespace BullsAndCowsTests
 		
         GameEngine gameEngine;
 
-        private const string WelcomeMessage = "Welcome to “Bulls and Cows” game. " +
-                                              "Please try to guess my secret 4-digit number." +
-                                              "Use 'top' to view the top scoreboard, 'restart' " +
-                                              "to start a new game and 'help' to cheat and 'exit' to quit the game.";
-        private const string EnterGuess = "Enter your guess or command: ";
-
         private GameEngine EngineSetup(string generateNumber, string helpPattern)
         {
             IRandomGenerator generator = new FakeNumberGenerator(generateNumber, helpPattern);
@@ -82,7 +76,7 @@ namespace BullsAndCowsTests
                               "1. tsvety --> 1 guess\n" +
                               WelcomeMessage +
                               EnterGuess +
-                              "Good Bye!"+Environment.NewLine);
+                              "Good Bye! "+ Environment.NewLine);
            
             Assert.AreEqual<string>(expected, result);
         }
@@ -90,7 +84,7 @@ namespace BullsAndCowsTests
         [TestMethod]
         public void PlayerRestartTheGame()
         {
-            var result = TestSetup("1234", "5678", "restart");
+            string result = TestSetup("1234", "5678", "restart\nexit");
 
             string expected =
                  string.Format(
@@ -102,7 +96,19 @@ namespace BullsAndCowsTests
         [TestMethod]
         public void PlayerEnterIncorrectCommand()
         {
+            string result = TestSetup("1234", "0123", "incorrectCommand");
 
+            string expected =
+                string.Format(WelcomeMessage +
+                              EnterGuess +
+                              "You entered incorrect command.\n" +
+                              "You can choose from one of the following commands: \n" +
+                              "'restart', 'help', 'top', 'exit'." +
+                              WelcomeMessage +
+                              EnterGuess +
+                              "Good Bye!" + Environment.NewLine);
+
+            Assert.AreEqual<string>(expected, result);
         }
     }
 }
